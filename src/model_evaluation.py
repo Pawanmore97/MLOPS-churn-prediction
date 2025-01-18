55f28023aaf1d3a8a4d8ec4b3cb386fe08c13685
pawanmore97 <pawan.more089@gmail.com>
2025-01-18 17:49:30 +0530

    model evaluation code added

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -4,14 +4,16 @@ import os
 import logging
 from sklearn.ensemble import RandomForestClassifier
 import pickle
+from sklearn.metrics import accuracy_score,precision_score,recall_score,confusion_matrix,f1_score,roc_auc_score
+
 
 log_dir = "logs"
 os.makedirs(log_dir,exist_ok=True)
 
-logger = logging.getLogger("model_trainer")
+logger = logging.getLogger("model_evaluation")
 logger.setLevel("DEBUG")
 
-log_file_path = os.path.join(log_dir,"model_trainer.log")
+log_file_path = os.path.join(log_dir,"model_evaluation.log")
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel("DEBUG")
 
@@ -20,48 +22,51 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
 
-def model_trainer(X_trian_path:str,y_train_path:str):
+def model_prediction(model_obj_path:str,test_data_path:str):
     try:
-        
-        X_train = pd.read_csv(X_trian_path)
-        y_train = pd.read_csv(y_train_path)
+        with open(model_obj_path,"rb") as f:
+            model = pickle.load(f)
 
-        rfc = RandomForestClassifier(
-            n_estimators= 50,
-            max_depth= 5,
-            min_samples_split= 7
-        )
+        X_test = pd.read_csv(test_data_path)
 
-        rfc.fit(X_train,y_train)
-        logger.info("Model fitted to data successfully!!!%s")
-        return rfc
+        y_pred = model.predict(X_test)
+        y_proba = model.predict_proba(X_test)
+
+
+        return y_pred
+    except Exception as e:
+        print(e)
+        logger.info("Unexcepected eroor -%s",e)
+    
+def evaluation(y_test,y_pred):
+    try:
+        accuracy = accuracy_score(y_test,y_pred)
+        Roc_Auc_score = roc_auc_score(y_test,y_pred)
+        print(f"accuracy score - {accuracy}")
+        print(f"roc auc score - {Roc_Auc_score}")
+
+        logger.info(f"accuracy score - {accuracy}")
+        logger.info(f"roc auc score - {Roc_Auc_score}")
 
     except Exception as e:
         print(e)
-        logger.info("Unexcepected error -%s",e)
-
-def save_model(model_obj,model_obj_path):
-
-    try:
-        model_dir_path = os.path.join(model_obj_path,"models")
-        os.makedirs(model_dir_path,exist_ok=True)
-        with open(os.path.join(model_dir_path,"rfc.pkl"),"wb") as f:
-            pickle.dump(model_obj,f)
-        logger.info("Model saved successfully!!!%s")
-    except Exception as e:
-        print(e) 
-        logger.info("Unexcepected error -%s",e)
+        logger.info("Unexcepected eroor -%s",e)
 
 def main():
     try:
-        X_train_path = "data/scaled_data/X_train_scl.csv"
-        y_train = "data/labels/y_train.csv"
-        rfc = model_trainer(X_train_path,y_train)
-        save_model(rfc,"./model_objects")
+        model_obj_path = "model_objects/models/rfc.pkl"
+        test_data_path = "data/scaled_data/X_test_scl.csv"
+        y_test_path = "data/labels/y_test.csv"
+
+        y_test = pd.read_csv(y_test_path)
+        y_test = np.array(y_test)
+        y_pred = model_prediction(model_obj_path,test_data_path)
+
+        evaluation(y_test,y_pred)
 
     except Exception as e:
         print(e)
-        logger.info("Unexcepected error -%s",e)
+        logger.info("Unexcepected eroor -%s",e)
 
 if __name__ == "__main__":
     main()
